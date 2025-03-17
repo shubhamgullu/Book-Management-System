e1ce62e11b7bfd3894bf803ac9c23f5e2381b4cb
shubham <shubhamgullu@gmail.com>
2025-03-17 20:24:29 +0530

    Create indexes using JPA

--- BookManagementSystem/src/main/java/com/book/Management/System/BookManagementSystem/entity/CountryMaster.java
+++ BookManagementSystem/src/main/java/com/book/Management/System/BookManagementSystem/entity/CountryMaster.java
@@ -6,17 +6,22 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "country_master")
+@Table(name = "country_master",indexes = {
+        @Index(name = "countryName",columnList = "countryName")
+})
+//@Index(name = "countaryName", columnList = "name")
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true) // This will ignore any fields that are not mapped in the entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+// This will ignore any fields that are not mapped in the entity
 public class CountryMaster extends BaseEntity {
 
-    @Column(name = "name", nullable = false)
-    @JsonProperty("name")
-    private String name;
+    @Column(name = "countryName", nullable = false)
+    @JsonProperty("countryName")
+    private String countryName;
 
     @Column(name = "phone_code", nullable = false)
     @JsonProperty("phoneCode")
--- BookManagementSystem/src/main/java/com/book/Management/System/BookManagementSystem/entity/StateMaster.java
+++ BookManagementSystem/src/main/java/com/book/Management/System/BookManagementSystem/entity/StateMaster.java
@@ -7,14 +7,18 @@ import javax.persistence.*;
 
 @Data
 @Entity
-@Table(name = "state_master")
+@Table(name = "state_master",indexes = {
+        @Index(name = "stateName",columnList = "state_name"),
+        @Index(name = "stateCode",columnList = "state_code"),
+        @Index(name = "state",columnList = "state_name, state_code",unique = true)
+})
 public class StateMaster extends BaseEntity {
 
     @Column(name = "state_name", nullable = false)
     private String stateName;
 
     @ManyToOne
-    @JoinColumn(name = "countary_id", nullable = false)
+    @JoinColumn(name = "country_id", nullable = false)
     private CountryMaster country;
 
     @Column(name = "status", nullable = false)
